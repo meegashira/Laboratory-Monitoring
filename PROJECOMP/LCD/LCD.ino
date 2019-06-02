ced5647951bd4f740d1d8019221d89aced46f828
Gasparrocha <42660754+Gasparrocha@users.noreply.github.com>
2019-06-02 00:22:01 -0300

    Arduino Uno and ESP28566 code
    "LCD.ino" contains c code for reading sensors, lcd usage and serial communication for data sending including general configuration for these systems.
    "esp.ino" contains c code for wifi conection, firebase comunication and serial communication for data receiving.

---
+++ PROJECOMP/LCD/LCD.ino
@@ -0,0 +1,144 @@
+#include <Adafruit_Sensor.h> 
+#include <Adafruit_BMP280.h>
+#include <LiquidCrystal.h>
+#include <DHT.h>
+
+//Constants
+#define pushButton 10
+#define gasSensor 13
+#define warningLED 6
+#define DHTPIN 7     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+//general declarations
+Adafruit_BMP280 bmp; 
+LiquidCrystal lcd(12,11,5,4,3,2);
+/*-LCD pins conection-*/
+/*1 .Vss_____GND      */
+/*2 .Vdd_____5V       */
+/*3 .V0______POT      */
+/*4 .Rs______pin 12   */
+/*5 .R/W_____GND      */
+/*6 .Enable__pin 11   */
+/*7 .DB0              */
+/*8 .DB1              */
+/*9 .DB2              */
+/*10.DB3              */
+/*11.DB4_____pin 5    */
+/*12.DB5_____pin 4    */
+/*13.DB6_____pin 3    */
+/*14.DB7_____pin 2    */
+/*15.Anodo___5V       */
+/*16.Catodo__GND      */
+/**********************/
+
+//global variables
+int lcdState=0;   //switch control
+int btnState=0;   //switch control
+
+float temperature;//Sensor data
+float pressure;   //Sensor data
+float humidity;   //Sensor data
+bool  gas;        //Sensor data
+
+unsigned long timer;  //time counter
+unsigned long dataBaseBaud=60000;//upload data baud rate
+String sendingStream; //String of data
+
+void setup() {
+  pinMode(pushButton, INPUT);
+  pinMode(gasSensor,  INPUT);
+  pinMode(warningLED, OUTPUT);
+  lcd.begin(16,2);            //initializing lcd
+  lcd.setCursor(0,0);         //seting cursor position
+  dht.begin();                //initializing dht
+  Serial.begin(115200);       //initializing serial com with esp28566
+  Serial.println(dataBaseBaud);
+  if(!bmp.begin(0x76)){       //initializing bpm280 if error do...
+    digitalWrite(warningLED,HIGH);
+    while(1);
+  }
+  timer=millis();
+}
+
+void loop() {    
+    if(digitalRead(pushButton)){        //pushButtom reading
+      delay(100);                       //debounce delay
+      btnState=digitalRead(pushButton);
+      if(btnState==0){                  //buttom released
+        lcdState++;                     //next state
+        if(lcdState>3) lcdState=0;//looping stat
+        lcd.clear();
+      }
+      //digitalWrite(warningLED,LOW);
+    }
+    readSensors();                    //reading all sensor values
+    if(millis()-timer>=dataBaseBaud){ //cheking data sending time
+      sendData();                     //sending data to esp28566
+      timer=millis();                 //refresh timer
+    }
+    switch(lcdState){
+      case 0:
+        lcd.setCursor(0,0);
+        lcd.print("Temperatura"); 
+        lcd.setCursor(0,1);
+        lcd.print(temperature);   
+        lcd.print("C");  
+        break;
+  
+      case 1:
+        lcd.setCursor(0,0);
+        lcd.print("Existe Gas:");
+        if(gas==1){
+          lcd.setCursor(0,1);
+          lcd.print("Nao");
+        }else{
+          lcd.setCursor(0,1);
+          lcd.print("Sim");      
+        }
+        break;
+
+      case 2:
+        lcd.setCursor(0,0);
+        lcd.print("Umidade");
+        lcd.setCursor(0,1);
+        lcd.print(humidity);
+        lcd.print("%");
+        break;      
+
+      case 3:
+        lcd.setCursor(0,0);
+        lcd.print("Pressao");
+        lcd.setCursor(0,1);
+        lcd.print(pressure);
+        lcd.print("Pa");
+        break;
+
+      default:
+        break;
+    }
+}
+
+void readSensors(){ //reading sensors
+  temperature=bmp.readTemperature();
+  pressure=bmp.readPressure();
+  humidity = dht.readHumidity();
+  gas = digitalRead(gasSensor);
+}
+
+void sendData(){
+  sendingStream=""; //Concatenate data in a string
+  sendingStream+=temperature;
+  sendingStream+=" ";
+  sendingStream+=humidity;
+  sendingStream+=" ";
+  sendingStream+=pressure;
+  sendingStream+=" ";
+  sendingStream+=gas;
+  sendingStream+='f';//end message char
+  Serial.print(sendingStream);//send data to esp28566
+  /*Serial.println(millis()-timer);
+  digitalWrite(warningLED,HIGH);*/
+}
+
---
+++ PROJECOMP/esp/esp.ino
@@ -0,0 +1,86 @@
+#include <ESP8266WiFi.h>                                                   
+#include <FirebaseArduino.h>
+#include <ArduinoJson.h>                                               
+#include <time.h>
+#include <string.h>
+//#include <SoftwareSerial.h>
+//SoftwareSerial arduino(D9,D10);
+unsigned long count1=0;
+#define FIREBASE_HOST "laboratory-monitoring-d1caa.firebaseio.com"
+#define FIREBASE_AUTH "T4LGKaSCBuoGOVTPGSkb4AMaP1WuR7I8pMbkC6zM"
+
+#define WIFI_SSID "Vc não sabe nem eu"
+#define WIFI_PASSWORD "joaozinho"
+
+String  timestamp;
+String  dataStream;
+String   rec;
+
+struct Data{
+  float humidity;
+  float temperature;
+  float pressure;
+  bool  gas;
+};
+
+Data reading;
+
+DynamicJsonBuffer jsonBuffer(200);
+//JsonObject& root = jsonBuffer.createObject();
+
+void setup() {
+  /*reading.temperature=25;
+  reading.humidity=50;
+  reading.pressure=12000;
+  reading.gas=false;*/
+  
+  Serial.begin(115200);
+  delay(1000);
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  configTime(-3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+}
+
+void loop() {
+  if(Serial.available()){
+    reading.temperature=Serial.readStringUntil(' ').toFloat();
+    reading.humidity=Serial.readStringUntil(' ').toFloat();
+    reading.pressure=Serial.readStringUntil(' ').toFloat();
+    reading.gas=Serial.readStringUntil('f').toInt();
+    uploadData(&reading);
+  }
+}
+
+void uploadData(struct Data * data){
+  
+  time_t now;
+  struct tm * timeinfo;
+  time(&now);
+  timeinfo = localtime(&now);
+  
+  timestamp=timeinfo->tm_year+1900;
+  timestamp+='-';
+  timestamp+=timeinfo->tm_mon+1;
+  timestamp+='-';
+  timestamp+=timeinfo->tm_mday;
+  timestamp+='T';
+  timestamp+=timeinfo->tm_hour;
+  timestamp+=':';
+  timestamp+=timeinfo->tm_min;
+  timestamp+=':';
+  timestamp+=timeinfo->tm_sec;
+  timestamp+='Z';
+
+  JsonObject& root = jsonBuffer.createObject();
+  root["temperature"]=data->temperature;
+  root["humidity"]=data->humidity;
+  root["atmPressure"]=data->pressure;
+  root["gas"]=data->gas;
+  root["tStamp"]=timestamp;
+  Firebase.push("Data",root);
+  
+}
+
